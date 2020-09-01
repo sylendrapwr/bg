79f2db8d6492402dd1aef16e5ad0156fc51d4265
fahmilukis <fahmilukis04@gmail.com>
2020-09-01 09:49:49 +0700

    function update on updater.sh

--- updater.sh
+++ updater.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 echo "check update ..."
 
+# download perbaruan
+function update {
+    git pull origin master
+    echo "update to new version"
+}
+
+# cek versi
 git config --global credential.helper store
 git fetch origin master
-
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $BASE ]; then
-    git pull origin master
-    echo "pull new version"
+    update
 else
     echo "error"
 fi
-
