89b968dfe44f0fa5d8cd0997ffa7a90ad237cbe5
fahmilukis <fahmilukis04@gmail.com>
2020-09-01 11:51:45 +0700

    delete run.sh

--- updater.sh
+++ updater.sh
@@ -7,6 +7,29 @@ function update {
     echo "update to new version"
 }
 
+# download
+function extract {
+    cat x* > file.zip
+    7z e file.zip
+}
+
+function removeDir {
+    rm ../program/*
+}
+
+function copyFile {
+    # copy serial number
+    cp ~/.serial_number.txt ../program
+    mv ui.AppImage ../program
+    mv api-golang ../program
+    mv api-python.py ../program 
+}
+
+# reboot
+function restart {
+    sudo reboot
+}
+
 # cek versi
 git config --global credential.helper store
 git fetch
@@ -16,6 +39,10 @@ REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $BASE ]; then
     update
-else
-    echo "error"
+    extract
+    removeDir
+    copyFile
 fi
+
+restart
+
